d45120b7e8ae62c69e35e7c69279b3a1270be3bb
Hyran <hyran.1301706@fapi-pinhais.edu.br>
2018-05-10 20:39:21 -0300

    FormaGeometrica totalmente funcional

--- src/main/java/formaGeometrica/Main.java
+++ src/main/java/formaGeometrica/Main.java
@@ -1,14 +1,26 @@
 package formaGeometrica;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o tamanho do lado do seu quadrado :)");
+        float ladoQuadrado = scanner.nextFloat();
+
         Quadrado quadrado = new Quadrado();
-        quadrado.setLado(10/4);
-        quadrado.setAltura(6/4);
-        quadrado.setBase(5/4);
+        quadrado.setLado(ladoQuadrado);
+        System.out.println("A area do quadrado é: " + quadrado.calcularArea());
+        System.out.println("O perímetro do quadrado é: " + quadrado.calcularPerimetro());
+
+
+        System.out.println("Por favor, digite o tamanho do raio do seu circulo >=]:)");
+        float raioCirculo = scanner.nextFloat();
 
         Circulo circulo = new Circulo();
-        circulo.setRaio(10/4);
+        circulo.setRaio(raioCirculo);
+        System.out.println("A area do circulo é: " + circulo.calcularArea());
+        System.out.println("O perímetro do circulo é: " + circulo.calcularPerimetro());
     }
 }
